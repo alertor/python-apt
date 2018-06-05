50f7bb203a1225d20d1cb0a97215d247fb4a94b5
Julian Andres Klode <jak@debian.org>
2018-06-05 16:04:28 -0700

    apt_pkg.DepCache: Raise CacheMismatchError if argument belongs to diff. cache
    This allows us to catch errors and recover from them.
    
    (cherry picked from commit 9c99bbcd915daa64542a0303cd09fd74300fdaa7)
    (cherry-picked from commit 4297bb6ebbf6b68f6b6550adeaf3a7abb512de69)
    (cherry picked from commit ab8203ce71456cf6a68b51ab6011e777c78715f3)

--- python/depcache.cc
+++ python/depcache.cc
@@ -35,7 +35,7 @@
 
 #define VALIDATE_ITERATOR(I) do { \
    if ((I).Cache() != &depcache->GetCache()) { \
-      PyErr_SetString(PyExc_ValueError, "Object of different cache passed as argument to apt_pkg.DepCache method"); \
+      PyErr_SetString(PyAptCacheMismatchError, "Object of different cache passed as argument to apt_pkg.DepCache method"); \
       return NULL; \
    } \
 } while(0)
